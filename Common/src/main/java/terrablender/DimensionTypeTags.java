82460179c5b61912d3e341dae444d3b54ff860bc
1980Spillo <tato.cherubini@gmail.com>
2025-03-07 19:31:02 +0100

    nightworld DimensionTypeTags.java

--- Common/src/main/java/terrablender/DimensionTypeTags.java
+++ Common/src/main/java/terrablender/DimensionTypeTags.java
@@ -11,6 +11,7 @@ public class DimensionTypeTags {
 
     public static final TagKey<DimensionType> OVERWORLD_REGIONS = create("overworld_regions");
     public static final TagKey<DimensionType> NETHER_REGIONS = create("nether_regions");
+    public static final TagKey<DimensionType> NIGHTWORLD_REGIONS = create("nightworld_regions");
 
     private static TagKey<DimensionType> create(String id) {
         return TagKey.create(Registries.DIMENSION_TYPE, new ResourceLocation(TerraBlender.MOD_ID, id));
